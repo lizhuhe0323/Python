5394ea7c7743c6b5cf4070f6b479a9fb5690bc94
lizhuhe <lizhuhe@163.com>
2018-03-28 15:35:20 +0800

    test

--- mkfile.py
+++ mkfile.py
@@ -1,21 +1,29 @@
 import os
 
-contents = []
+def get_fname():
+    while True:
+        fname = input("Please input filename:")
+        if not os.path.exists(fname):
+            break
+        print ("%s already exists.Tay again." % fname)
+    return fname
 
-while True:
-    fname = input("Please input filename:")
-    if not os.path.exists(fname):
-        break
-    print ("%s already exists.Tay again." % fname)
+def get_contents():
+    contents = []
+    while True:
+        data = input("(Enter to quit)>")
+        if not data:
+            break
+        contents.append(data + '\n')
+    return contents
 
-while True:
-    data = input("(Enter to quit)>")
-    if not data:
-        break
-    contents.append(data + '\n')
+def wfile(fname,contents):
+    fobj = open(fname,'w')
+    fobj.writelines(contents)
+    fobj.close()
 
-fobj = open(fname,'w')
-fobj.writelines(contents)
-fobj.close()
-
+if __name__ == '__main__':
+    filename = get_fname()
+    lines = get_contents()
+    wfile(filename,lines)
 
