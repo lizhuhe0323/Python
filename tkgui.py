8209d98c08ea86f59ff47175e8c3fdbc079e2b2a
lizhuhe <lizhuhe@163.com>
2018-04-02 00:05:04 +0800

    test

--- tkgui.py
+++ tkgui.py
@@ -1,12 +1,13 @@
 import tkinter
 import os
 from game import run_game
+from login import *
 
 top = tkinter.Tk()
 hello =tkinter.Label(top,text='Hello World!')
 hello.pack()
 
-login = tkinter.Button(text='登陆',command=run_game,bg='red',fg='white')
+login = tkinter.Button(text='登陆',command=show_menu,bg='red',fg='white')
 login.pack()
 quit = tkinter.Button(top,text='QUIT',
                       command=top.quit,bg='red',fg='white')
