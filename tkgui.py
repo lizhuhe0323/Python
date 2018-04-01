725fdc8f0fd2d74897152e9231b14ee0e2390d59
lizhuhe <lizhuhe@163.com>
2018-04-02 00:28:37 +0800

    test

--- tkgui.py
+++ tkgui.py
@@ -3,14 +3,20 @@ import os
 from game import run_game
 from login import *
 
+
 top = tkinter.Tk()
-hello =tkinter.Label(top,text='Hello World!')
+top.title('1944')
+top.geometry('500x300+500+200')
+top.iconbitmap('spider_128px_1169260_easyicon.net.ico')
+hello =tkinter.Label(top,text='1944打飞机游戏')
 hello.pack()
 
 login = tkinter.Button(text='登陆',command=show_menu,bg='red',fg='white')
 login.pack()
-quit = tkinter.Button(top,text='QUIT',
+score = tkinter.Button(text='最高分数')
+score.pack()
+quit = tkinter.Button(top,text='退出',
                       command=top.quit,bg='red',fg='white')
-quit.pack(fill=tkinter.X,expand=1)
+quit.pack()
 
 tkinter.mainloop()
