fcbf3093115eafdad89605ec3a88b92e73ebf79e
lizhuhe <lizhuhe@163.com>
2018-04-03 22:43:00 +0800

    test

--- backup.py
+++ backup.py
@@ -28,13 +28,33 @@ def full_backup(src_dir,dst_dir,md5file):
     for path,dirs,files in os.walk(src_dir):
         for each_file in files:
             full_name = os.path.join(path,each_file)
-            md5dict[full_name] = check_md5(each_file)
+            md5file[full_name] = check_md5(each_file)
 
     with open(md5file,'w') as fobj:
         p.dump(md5dict,fobj)
 
 def incr_backup(src_dir,dst_dir,md5file):
-    pass
+    base_dir, back_dir = os.path.split(src_dir.rstrip('/'))
+    back_name = "%s_incr_%s.tar.gz" % (back_dir, time.strftime('%Y%m%d'))
+    full_path = os.path.join(dst_dir, back_name)
+    new_md5 = {}
+    with open(md5file) as fobj:
+        old_md5 = p.load(fobj)
+
+    for path,dirs,files in os.walk(src_dir):
+        for each_file in files:
+            full_name = os.path.join(path,each_file)
+            new_md5[full_name] = check_md5(each_file)
+
+    with open(md5file,'w') as fobj:
+        p.dump(new_md5,fobj)
+
+    os.chdir(base_dir)
+    tar = tarfile.open(full_path)
+    for key in new_md5:
+        if key not in old_md5 or new_md5[key] != old_md5[key]:
+            tar.add(key.split((base_dir)[1].lstrip('/')))
+    tar.close()
 
 if __name__ == '__main__':
     src_dir = '/home/demo'
