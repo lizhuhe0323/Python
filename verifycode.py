56de352a8f12d721163643f5240e4782239f0adf
lizhuhe@163.com <Ab@12345>
2018-04-03 15:58:04 +0800

    ok

--- verifycode.py
+++ verifycode.py
@@ -2,12 +2,21 @@
 import os
 import pygame
 from pygame.locals import *
+import random
 
+seed = "1234567890abcdefghigklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ"
 pygame.init()
-text = u"PythonTab中文网"
+text = str(random.choice(seed)+random.choice(seed)+random.choice(seed)+random.choice(seed))
 #设置字体和字号
 font = pygame.font.SysFont('Microsoft YaHei', 64)
 #渲染图片，设置背景颜色和字体样式,前面的颜色是字体颜色
 ftext = font.render(text, True, (65, 83, 130),(255, 255, 255))
 #保存图片
 pygame.image.save(ftext, "/home/lizhuhe/pythontab.jpg")
+
+typein = input('input your verify code:')
+
+if typein == text:
+    print ("OK!")
+else:
+    print ("Error!")
