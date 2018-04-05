9b2d45d55f58a99e3d143dcb828f1429c3014fff
lizhuhe <lizhuhe@163.com>
2018-04-05 21:17:01 +0800

    test

--- classtemp.py
+++ classtemp.py
@@ -1,14 +1,23 @@
-class Myclass:
-    number = 123456
+class AddrBook(object):
+    def __init__(self,nm,ph):
+        self.name = nm
+        self.phone = ph
+        self.city = "Beijing"
 
-    def __init__(self):
-        self.data = []
+    def get_name(self):
+        return self.name
 
-    def f(self):
-        return 'hello,world'
+    def get_phone(self):
+        return self.phone
 
-x = Myclass()
+    def update_phone(self,newph):
+        self.phone = newph
+        print("Now,%s phone number is: %s" % (self.name,self.phone))
 
-print("Myclass类的属性number为：",x.number )
-print("Myclass类的方法f的输出为:",x.f())
-
+if __name__ == '__main__':
+    bob = AddrBook("Bob Green","15011111111")
+    alice = AddrBook("Alice Smith","14511111221")
+    print ("%s:%s" % (bob.get_name(),bob.get_phone()))
+    print ("%s,%s" % (alice.get_name(),alice.get_phone()))
+    bob.update_phone("15555555555")
+    print ("%s:%s" % (bob.get_name(),bob.get_phone()))
