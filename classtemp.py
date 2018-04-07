20ae7f3fddb0233daec0ec58a51c7c1ec034594e
lizhuhe <lizhuhe@163.com>
2018-04-07 15:27:35 +0800

    test

--- classtemp.py
+++ classtemp.py
@@ -14,6 +14,18 @@ class AddrBook(object):
         self.phone = newph
         print("Now,%s phone number is: %s" % (self.name,self.phone))
 
+class EmplAddrBook(AddrBook):
+    company = "Tarena"
+
+    def __init__(self,nm,ph,em,eid):
+        AddrBook.__init__(self,nm,ph)
+        self.email = em
+        self.eid = eid
+        aaa = 10
+
+    def get_email(self):
+        return self.email
+
 if __name__ == '__main__':
     bob = AddrBook("Bob Green","15011111111")
     alice = AddrBook("Alice Smith","14511111221")
@@ -21,3 +33,6 @@ if __name__ == '__main__':
     print ("%s,%s" % (alice.get_name(),alice.get_phone()))
     bob.update_phone("15555555555")
     print ("%s:%s" % (bob.get_name(),bob.get_phone()))
+    luke = EmplAddrBook('luke li','18611111111',"luke.li@test.com","1088")
+    print (EmplAddrBook.company)
+    print (luke.company)
